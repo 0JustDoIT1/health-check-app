887f2ffd52a413bc18bddb4be19a43949969bef7
0justdoit1 <0justdoit1@gmail.com>
2026-04-16 19:31:17 +0900

    template

--- routes/auth.py
+++ routes/auth.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 auth_bp = Blueprint('auth', __name__)
 
-@app.route("/login")
-def login():
-    return render_template("auth/login.html")
+@auth_bp.route("/signin")
+def signin():
+    return render_template("auth/signin.html")

fed0dbc9436fc8c9f4d6d76d9c5b12d15c63c069
0justdoit1 <0justdoit1@gmail.com>
2026-04-17 00:23:43 +0900

    test

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from routes.user_routes import user_bp
+from routes.auth_routes import auth_bp
 
 app = Flask(__name__)
 
 # Blueprint 등록
-app.register_blueprint(user_bp)
+app.register_blueprint(auth_bp, url_prefix="/auth")
 
 if __name__ == "__main__":
     app.run(debug=True)
